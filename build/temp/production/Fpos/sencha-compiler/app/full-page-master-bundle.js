f7b8af5b20a04d9ce56b91f7af7c027a5c26d91c
Funkring <martin.reisenhofer@funkring.net>
2016-01-07 14:52:39 +0100

    [FIX] removed build

--- build/temp/production/Fpos/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,6 +0,0 @@
-// @tag full-page
-// @require /home/martin/Work/odoo/fpos/lib/futil.js
-// @require /home/martin/Work/odoo/fpos/lib/openerplib.js
-// @require /home/martin/Work/odoo/fpos/lib/pouchdb-5.1.0.min.js
-// @require /home/martin/Work/odoo/fpos/lib/URI.js
-// @require /home/martin/Work/odoo/fpos/app.js
