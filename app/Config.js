e93f7a980f321289ff88edb78c5e6bd094dac5eb
Martin Reisenhofer <martin.reisenhofer@funkring.net>
2016-03-15 11:16:17 +0100

    [VERSION] 3.0.2

--- app/Config.js
+++ app/Config.js
@@ -12,7 +12,7 @@ Ext.define('Fpos.Config', {
         'Ext.form.ViewManager'
     ],
     config : {       
-        version : '3.0.1',
+        version : '3.0.2',
         log : 'Ext.store.LogStore',
         databaseName : 'fpos',  
         searchDelay : 500,
