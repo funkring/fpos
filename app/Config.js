c45de1cd7453424d5f934725563a5f8a0a5c908f
Martin Reisenhofer <martinr@oerp.at>
2016-10-03 01:02:52 +0200

    [VERSION] 4.0.12

--- app/Config.js
+++ app/Config.js
@@ -14,7 +14,7 @@ Ext.define('Fpos.Config', {
         'Fpos.core.HwProxy'
     ],
     config : {       
-        version : '4.0.11',
+        version : '4.0.12',
         log : 'Ext.store.LogStore',
         databaseName : 'fpos',  
         searchDelay : 500,
