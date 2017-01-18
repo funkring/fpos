117d5cbd955141d9dc4d73fcd8ca1430320a5090
Martin Reisenhofer <martinr@oerp.at>
2017-01-18 20:56:19 +0100

    [BUILD] VERsION 4.0.32

--- app/Config.js
+++ app/Config.js
@@ -16,7 +16,7 @@ Ext.define('Fpos.Config', {
         'Fpos.model.OPartner'
     ],
     config : {       
-        version : '4.0.31',
+        version : '4.0.33',
         log : 'Ext.store.LogStore',
         databaseName : 'fpos',  
         searchDelay : 500,
