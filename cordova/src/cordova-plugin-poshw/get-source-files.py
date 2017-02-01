9325023d958a1b6c65ca26312ade68ac719f124c
Martin Reisenhofer <martinr@oerp.at>
2017-02-01 19:04:29 +0100

    [IMPL] better get-source-files

--- cordova/src/cordova-plugin-poshw/get-source-files.py
+++ cordova/src/cordova-plugin-poshw/get-source-files.py
@@ -2,6 +2,7 @@
 
 import glob
 import os
+from xml.etree import ElementTree
 
 source = [
    "cordova/android/src",
@@ -18,8 +19,13 @@ drawable = [
 
 process = [(source,"src"), (libs,"libs"), (drawable,"res/drawable")]
 
-print
+# read source
+sourceFiles = set()
+pluginRoot = ElementTree.parse("plugin.xml")
+for sourceFile in pluginRoot.findall(".//{http://apache.org/cordova/ns/plugins/1.0}source-file"):
+    sourceFiles.add(sourceFile.get("src"))
 
+# output
 for srclist, dest in process:
     for src in srclist:
         for root, directories, files in os.walk(src):
@@ -28,7 +34,6 @@ for srclist, dest in process:
                     continue
                 source_path = os.path.join(root, file)
                 dest_path = os.path.dirname(os.path.join(dest,source_path[len(src)+1:]))
-                #if dest_path.endswith("armeabi"):
-                #    dest_path = "armeabi-v7a"
-                print '<source-file src="%s" target-dir="%s" />' % (source_path, dest_path+"/")
+                if not source_path in sourceFiles:
+                    print '\t\t<source-file src="%s" target-dir="%s" />' % (source_path, dest_path+"/")
     print
