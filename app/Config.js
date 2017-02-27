ec2aae1624da66dd54df9d9f9e2dc43b70ce95fe
Martin Reisenhofer <martin.reisenhofer@funkring.net>
2017-02-27 19:39:34 +0100

    [VERSION] 5.0.2

--- app/Config.js
+++ app/Config.js
@@ -16,7 +16,7 @@ Ext.define('Fpos.Config', {
         'Fpos.model.OPartner'
     ],
     config : {       
-        version : '5.0.1',
+        version : '5.0.2',
         log : 'Ext.store.LogStore',
         databaseName : 'fpos',  
         searchDelay : 500,
