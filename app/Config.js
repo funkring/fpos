86ccc6d052f4707e7a0d781b7a7635782030ef75
Martin Reisenhofer <martinr@oerp.at>
2017-06-06 13:41:30 +0200

    [VERSION] 5.0.18

--- app/Config.js
+++ app/Config.js
@@ -16,7 +16,7 @@ Ext.define('Fpos.Config', {
         'Fpos.model.OPartner'
     ],
     config : {       
-        version : '5.0.17',
+        version : '5.0.18',
         log : 'Ext.store.LogStore',
         databaseName : 'fpos',  
         searchDelay : 500,
